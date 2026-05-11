5d6ddbbf8d10e4058c04b99b7bc88798d0dc4379
marieauwu <mariea2205101527@diu.edu.bd>
2026-05-11 22:53:34 +0600

    Fix: base path and css issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: 'export', // স্ট্যাটিক বিল্ডের জন্য
+  basePath: '/marieauwu-portfolio',
+  assetPrefix: '/marieauwu-portfolio/',
   images: {
     unoptimized: true,
   },
